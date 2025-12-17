b41f582313ae72a498f98fac3a737d8d1c18f9bf
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2025-12-17 17:37:59 +0530

    Revert "Clear cache on PRE events."
    This reverts commit a3b29797c7496ddc223fec1d6ee7a759ad82d8ad.

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/listener/DefaultRoleManagementListener.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/listener/DefaultRoleManagementListener.java
@@ -697,9 +697,11 @@ public class DefaultRoleManagementListener extends AbstractApplicationMgtListene
     }
 
     @Override
-    public boolean doPreUpdateApplication(ServiceProvider serviceProvider, String tenantDomain, String userName)
+    public boolean doPostUpdateApplication(ServiceProvider serviceProvider, String tenantDomain, String userName)
             throws IdentityApplicationManagementException {
 
+        // Clear role basic info cache when application is updated, This is necessary because RoleBasicInfo contains
+        // audienceName (application name). When application name changes, cached role basic info becomes stale.
         RoleManagementUtils.clearRoleBasicInfoCacheByTenant(tenantDomain);
         if (LOG.isDebugEnabled()) {
             LOG.debug("Cleared role basic info cache for tenant: " + tenantDomain +
