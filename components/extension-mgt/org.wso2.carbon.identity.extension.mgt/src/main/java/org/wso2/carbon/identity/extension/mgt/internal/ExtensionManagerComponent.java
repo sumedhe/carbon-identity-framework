d52cf09bde293f8b1402f4710d55e475ebbde0fb
sadilchamishka <sandilchamishka@gmail.com>
2026-03-05 09:03:03 +0530

    Log error if extensions path directory is missing

--- components/extension-mgt/org.wso2.carbon.identity.extension.mgt/src/main/java/org/wso2/carbon/identity/extension/mgt/internal/ExtensionManagerComponent.java
+++ components/extension-mgt/org.wso2.carbon.identity.extension.mgt/src/main/java/org/wso2/carbon/identity/extension/mgt/internal/ExtensionManagerComponent.java
@@ -131,9 +131,7 @@ public class ExtensionManagerComponent {
 
             // Check whether the given extension type directory exists.
             if (!Files.exists(path) || !Files.isDirectory(path)) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Default templates directory does not exist: " + path);
-                }
+                log.error("Default templates directory does not exist: " + path);
                 continue;
             }
 
